1ceae8a50ccb29044d104655fe9a51f7169398f3
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-07-31 13:44:39 +0200

    A&D - User logic

---
+++ src/logic/users.js
@@ -0,0 +1,56 @@
+export const getUsers = () => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: 'Get Users'
+  }
+}
+
+export const getUser = userId => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: {
+      message: 'Get User',
+      userId: +userId
+    }
+  }
+}
+
+export const getUsersByGenre = () => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: 'Get Users by Genre'
+  }
+}
+
+export const getUsersByGroup = () => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: 'Get Users by Group'
+  }
+}
+
+export const postUser = user => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: {
+      message: 'Post User',
+      user
+    }
+  }
+}
+
+export const deleteUser = userId => {
+  // Llamada a la BD
+  return {
+    status: 200,
+    data: {
+      message: 'Delete User',
+      userId: +userId
+    }
+  }
+}
