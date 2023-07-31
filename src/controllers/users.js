a31b05fb91827b0b1491b65f24ea781c06710f6d
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-07-31 13:43:58 +0200

    A&D - User controllers

---
+++ src/controllers/users.js
@@ -0,0 +1,39 @@
+import {
+  getUsers as getUsersLogic,
+  getUser as getUserByLogic,
+  getUsersByGenre as getUsersByGenreLogic,
+  getUsersByGroup as getUserByGroupLogic,
+  postUser as postUserLogic,
+  deleteUser as deleteUserLogic
+} from '../logic/index.js'
+
+export const getUsers = (req, res) => {
+  const { status, data } = getUsersLogic()
+  res.status(status).send(data)
+}
+
+export const getUser = (req, res) => {
+  const { status, data } = getUserByLogic(req.params.userId)
+  res.status(status).send(data)
+}
+
+export const getUsersByGenre = (req, res) => {
+  const { status, data } = getUsersByGenreLogic()
+  res.status(status).send(data)
+}
+
+export const getUsersByGroup = (req, res) => {
+  const { status, data } = getUserByGroupLogic()
+  res.status(status).send(data)
+}
+
+export const postUser = (req, res) => {
+  console.log(req.body)
+  const { status, data } = postUserLogic(req.body.user)
+  res.status(status).send(data)
+}
+
+export const deleteUser = (req, res) => {
+  const { status, data } = deleteUserLogic(req.params.userId)
+  res.status(status).send(data)
+}
