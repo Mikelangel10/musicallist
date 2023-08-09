0ab11de029d8d3afc2052633e073ca6ab5de8bdf
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-09 10:38:02 +0200

    U - Create genre request

--- src/controllers/genres.js
+++ src/controllers/genres.js
@@ -1,6 +1,6 @@
 import { postGenres as postGenreLogic } from '../logic/genres.js'
 
-export const postGenres = (req, res) => {
-  const { status, data } = postGenreLogic(req.body.genre)
+export const postGenres = async (req, res) => {
+  const { status, data } = await postGenreLogic(req.body.genre)
   res.status(status).send(data)
 }
--- src/logic/genres.js
+++ src/logic/genres.js
@@ -1,10 +1,32 @@
-export const postGenres = genre => {
-  // Llamada a la BD
-  return {
-    status: 200,
-    data: {
-      message: 'Post Genre',
-      genre
+import Genre from '../models/genre.js'
+
+export const postGenres = async genre => {
+  try {
+    const newGenre = new Genre({ name: genre.name })
+    await newGenre.save()
+
+    const res = {
+      status: 201,
+      data: {
+        newGenre
+      }
+    }
+
+    return res
+  } catch (error) {
+    if (error.code === 11000)
+      return {
+        status: 400,
+        data: {
+          message: 'Genre already exits'
+        }
+      }
+
+    return {
+      status: 500,
+      data: {
+        message: 'Internal server error'
+      }
     }
   }
 }
