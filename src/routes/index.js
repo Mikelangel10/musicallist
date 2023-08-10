304f241f5573cda32bed10c0efb5ab36af2d2413
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-08-10 10:05:04 +0200

    R - Index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,12 @@
 import { Router } from 'express'
-import users from './users.js'
 import genres from './genres.js'
 import groups from './groups.js'
+import users from './users.js'
 
 const router = Router()
 
-router.use('/users', users)
 router.use('/genres', genres)
 router.use('/groups', groups)
+router.use('/users', users)
 
 export default router
