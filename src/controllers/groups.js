6de54c751d503d19b0e1a8bdde745696d7f33bbf
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2023-08-27 18:22:09 +0200

    U&D - Refactor group controller

--- src/controllers/groups.js
+++ src/controllers/groups.js
@@ -5,17 +5,21 @@ import {
 } from '../logic/groups.js'
 
 export const getGroups = async (req, res) => {
-  const { group } = req.query
-  const { status, data } = await getGroupsLogic(group)
+  // Constante innecesaria porque sólo se usa una vez
+  // const { group } = req.query
+  const { status, data } = await getGroupsLogic(req.query.group)
   res.status(status).send(data)
 }
 
 export const postGroup = async (req, res) => {
-  const result = validateGroup(req.boy.group)
+  // Constante necesaria porque se usa más de una vez
+  const { group } = req.body
+  // const result = validateGroup(req.boy.group) <- Línea anterior a la corrección: "req.boy.group" tiene un error. Hay que fijarse en esas cosas. Debes configurar correctamente el linter para que te avise de estos errores.
+  const result = validateGroup(group)
   if (!result.sucess)
     return res.status(400).json({ error: JSON.parse(result.error.message) })
 
-  const { status, data } = await postGroupLogic(req.body.group)
+  const { status, data } = await postGroupLogic(group)
   res.status(status).send(data)
 }
 
