3828280c0319daee131bdd67c6653fbe4e6620a0
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-05 18:16:05 +0200

    U - Add file password to userSchema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -10,6 +10,12 @@ export const userSchema = z.object({
     })
     .min(3, 'Name must be at least 3 characters long')
     .max(20, 'Name must be at most 20 characters long'),
+  password: z
+    .string({
+      required_error: 'Password is required'
+    })
+    .min(8, 'Password must be at least 8 characters long')
+    .max(20, 'Password must be at most 20 characters long'),
   email: z
     .string({
       required_error: 'Email is required'
