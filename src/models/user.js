3d6eda34e774db91b5e3ebd92debe75665ee1ca7
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-05 18:11:56 +0200

    U - File password add to userSchema

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ import { Schema, model } from 'mongoose'
 
 const userSchema = new Schema({
   name: { type: String, require: true },
+  password: { type: String, require: true },
   email: { type: String, require: true, unique: true },
   genres: [{ ref: 'Genre', type: Schema.Types.ObjectId }],
   groups: [{ ref: 'Group', type: Schema.Types.ObjectId }]
