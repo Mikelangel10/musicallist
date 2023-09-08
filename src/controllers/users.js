2d526c170aa76baa3eab243de2389e0d547e3c01
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-08 14:48:34 +0200

    U - Import postLoginuser,user & validate. Check if user exits, if the password exits, if the password match, login the user

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -3,6 +3,7 @@ import {
   getUser as getUserByLogic,
   getUsersByGenre as getUsersByGenreLogic,
   getUsersByGroup as getUserByGroupLogic,
+  postLoginUser as postLoginUserLogic,
   postUser as postUserLogic,
   putUser as putUserLogic,
   putUserGenre as putUserGenreLogic,
@@ -10,6 +11,8 @@ import {
   deleteUser as deleteUserLogic
 } from '../logic/users.js'
 import { validatePartialUser, validateUser } from '../schemas/user.js'
+import User from '../models/user.js'
+import { validate } from '../utils/bcrypt.js'
 
 export const getUsers = async (req, res) => {
   const { status, data } = await getUsersLogic()
@@ -29,6 +32,28 @@ export const getUsersByGenre = async (req, res) => {
 export const getUsersByGroup = async (req, res) => {
   const { status, data } = await getUserByGroupLogic(req.params.groupName)
   res.status(status).send(data)
+}
+
+export const postLoginUser = async (req, res) => {
+  const { email, password } = req.body.user
+  const result = validatePartialUser({ email, password })
+  if (!result.success)
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+
+  const user = await User.findOne({ email: req.body.user.email })
+
+  if (!user) return res.status(404).send({ message: 'User not found' })
+
+  if (!user.password)
+    return res.status(404).send({ message: 'Password not found in DB' })
+
+  if (!(await validate(password, user.password)))
+    return res.status(400).send({ message: 'Password not match' })
+
+  if (user) {
+    const { status, data } = await postLoginUserLogic(req.body.user)
+    return res.status(status).send(data)
+  }
 }
 
 export const postUser = async (req, res) => {
