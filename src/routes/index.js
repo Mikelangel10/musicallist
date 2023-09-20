98420250eb97417fe06f0da81cd0e51454cbe264
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-20 19:28:27 +0200

    U - Auth route add to the others.

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from 'express'
 import genres from './genres.js'
 import groups from './groups.js'
 import users from './users.js'
+import auth from './auth.js'
 
 const router = Router()
 
 router.use('/genres', genres)
 router.use('/groups', groups)
 router.use('/users', users)
+router.use('/auth', auth)
 
 export default router
