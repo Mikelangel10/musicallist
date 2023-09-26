2e63ff999173e5135d3d6fb35469774e01586a72
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-26 17:20:23 +0200

    A - Login route created

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,8 @@
 import { Router } from 'express'
 import { getToken } from '../controllers/auth.js'
-
+import { postLoginUser } from '../controllers/auth.js'
 const router = Router()
 
 router.get('/token', getToken)
-
+router.post('/login', postLoginUser)
 export default router
