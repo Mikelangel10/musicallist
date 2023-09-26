c962ee8c4e1adc41891d3f981ef77f49f6079ffc
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-26 17:24:53 +0200

    A - Controller of login users created

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,18 @@
 import { getToken as getToKenLogic } from '../logic/auth.js'
+import { postLoginUser as postLoginUserLogic } from '../logic/auth.js'
+import { validatePartialUser } from '../schemas/user.js'
 
 export const getToken = async (req, res) => {
   const { status, data } = await getToKenLogic(req.body.userId)
+  res.status(status).send(data)
+}
+
+export const postLoginUser = async (req, res) => {
+  const { email, password } = req.body.user
+  const result = validatePartialUser({ email, password })
+  if (!result.success)
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+
+  const { status, data } = await postLoginUserLogic(email, password)
   res.status(status).send(data)
 }
