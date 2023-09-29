1d94bd6fbeae7389844026eeda501071fbaaa8e7
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-29 17:35:27 +0200

    U - Create a new route to add groups by id to user id

--- src/routes/groups.js
+++ src/routes/groups.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
-import { getGroups, postGroup, deleteGroup } from '../controllers/groups.js'
+import {
+  getGroups,
+  postGroup,
+  deleteGroup,
+  addGroupByIdToUserById
+} from '../controllers/groups.js'
 
 const router = Router()
 
 router.get('/', getGroups)
 router.post('/', postGroup)
 router.delete('/:groupId', deleteGroup)
+router.put('/:groupId', addGroupByIdToUserById)
 
 export default router
