fc6b243f54c099a3e6e78aafb845603d0596c938
Miguel Angel <ma.garcianoguera@hotmail.com>
2023-09-29 18:48:32 +0200

    U - Create a new route to add a Genre by Id to an user by Id

--- src/routes/genres.js
+++ src/routes/genres.js
@@ -1,10 +1,16 @@
 import { Router } from 'express'
-import { getGenres, postGenre, deleteGenre } from '../controllers/genres.js'
+import {
+  getGenres,
+  postGenre,
+  deleteGenre,
+  addGenreByIdToUserById
+} from '../controllers/genres.js'
 
 const router = Router()
 
 router.get('/', getGenres)
 router.post('/', postGenre)
 router.delete('/:genreId', deleteGenre)
+router.put('/:genreId', addGenreByIdToUserById)
 
 export default router
